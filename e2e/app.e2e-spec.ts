af6ddd37ac4cd15a43e25973d47dd764f3736d54
Trisha C N <trisha.natanael@gmail.com>
2017-08-22 10:32:52 +0800

    first commit

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { AppPage } from './app.po';
+
+describe('sa44-weather-app-ca-master App', () => {
+  let page: AppPage;
+
+  beforeEach(() => {
+    page = new AppPage();
+  });
+
+  it('should display welcome message', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('Welcome to app!');
+  });
+});
